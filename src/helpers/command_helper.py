834a09e6c849f97c9783ed24ead0663d17ff5ac1
Jason Liu <jasonliu2468@hotmail.com>
2025-08-02 23:45:25 -0400

    Generically handle commands

---
+++ src/helpers/command_helper.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+from helpers.message_scheduler.mongo_utils import *
+from helpers.message_utils import *
+
+
+async def handle_command(cmd, interaction: discord.Interaction, allowed_roles):
+    # role check
+    if not any(role.id in allowed_roles for role in interaction.user.roles):
+        await interaction.response.send_message(
+            "Error: insufficient role", ephemeral=True
+        )
+        return
+
+    # creating a schedule and message object for new servers
+    await register_server_with_DB(ctx)
+    await interaction.response.send_message("Add")
+    # interpreting commands
+    try:
+        cmd()
+    except ValueError as e:  # this only throws if the user provided invalid arguments
+        await send_embedded_message(ctx, 0xFF0000, {"title": "ERROR", "desc": e})
+    except TypeError as e:
+        await send_embedded_message(ctx, 0xFF0000, {"title": "ERROR", "desc": e})
+    except RuntimeError as e:
+        await send_embedded_message(ctx, 0xFF0000, {"title": "ERROR", "desc": e})
+    except Exception as e:
+        Logger.error(e)
+        await send_embedded_message(
+            ctx,
+            0xFF0000,
+            {
+                "title": "ERROR",
+                "desc": "An error occurred. Command will be ignored.",
+            },
+        )
