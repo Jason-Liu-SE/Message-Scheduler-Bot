54b5f0bc1d2598f8429d5f08f9d3f8780019819d
Jason Liu <jasonliu2468@hotmail.com>
2025-08-02 21:09:35 -0400

    Remove colons from logger messages

--- src/helpers/logger.py
+++ src/helpers/logger.py
@@ -7,14 +7,12 @@ class Logger:
 
     @staticmethod
     def info(message):
-        print(f"[INFO]: {message}")
+        print(f"[INFO] {message}")
 
     @staticmethod
     def warn(message):
-        print(
-            f"{Logger.colours["WARNING"]}[WARNING]: {message}{Logger.colours["ENDC"]}"
-        )
+        print(f"{Logger.colours["WARNING"]}[WARNING] {message}{Logger.colours["ENDC"]}")
 
     @staticmethod
     def error(message):
-        print(f"{Logger.colours["ERROR"]}[ERROR]: {message}{Logger.colours["ENDC"]}")
+        print(f"{Logger.colours["ERROR"]}[ERROR] {message}{Logger.colours["ENDC"]}")
