65bb10b050d4ec96d0f47494c40f0876d44b0195
Jason Liu <jasonliu2468@hotmail.com>
2025-08-02 21:28:32 -0400

    Change logger message format

--- src/helpers/logger.py
+++ src/helpers/logger.py
@@ -1,18 +1,37 @@
+from datetime import datetime
+
+
 class Logger:
-    colours = {
-        "WARNING": "\033[93m",
-        "ERROR": "\033[91m",
+    __colours = {
+        "BLUE": "\033[94m",
+        "YELLOW": "\033[93m",
+        "RED": "\033[91m",
+        "DARKGRAY": "\033[90m",
         "ENDC": "\033[0m",
+        "BOLD": "\033[1m",
     }
 
     @staticmethod
+    def __get_date():
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    @staticmethod
     def info(message):
-        print(f"[INFO] {message}")
+        print(
+            f"{Logger.__colours["BOLD"]}{Logger.__colours["DARKGRAY"]}{Logger.__get_date()} "
+            + f"{Logger.__colours["BLUE"]}INFO\t{Logger.__colours["ENDC"]} {message}"
+        )
 
     @staticmethod
     def warn(message):
-        print(f"{Logger.colours["WARNING"]}[WARNING] {message}{Logger.colours["ENDC"]}")
+        print(
+            f"{Logger.__colours["BOLD"]}{Logger.__colours["DARKGRAY"]}{Logger.__get_date()} "
+            + f"{Logger.__colours["YELLOW"]}WARNING\t{message}{Logger.__colours["ENDC"]}"
+        )
 
     @staticmethod
     def error(message):
-        print(f"{Logger.colours["ERROR"]}[ERROR] {message}{Logger.colours["ENDC"]}")
+        print(
+            f"{Logger.__colours["BOLD"]}{Logger.__colours["DARKGRAY"]}{Logger.__get_date()} "
+            + f"{Logger.__colours["RED"]}ERROR\t{message}{Logger.__colours["ENDC"]}"
+        )
