6c81afcabaa94a87f37ad562cbc49b3e3f6066a5
Jason Liu <jasonliu2468@hotmail.com>
2025-08-03 22:32:06 -0400

    Add development mode validation

--- src/helpers/validate.py
+++ src/helpers/validate.py
@@ -1,3 +1,12 @@
-async def validate_channel(channel: str) -> None:
+import os
+
+
+def validate_channel(channel: str) -> None:
     if not channel.isdigit():  # ensure that the provided value could be a channel
         raise ValueError("The channel must be a numerical value")
+
+
+def is_development() -> None:
+    is_dev = os.getenv("IS_DEV")
+
+    return is_dev != None and is_dev.lower() == "true"
