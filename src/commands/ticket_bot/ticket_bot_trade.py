cbb33a97436a528bb07d1dc8d0fe98676515e955
Jason Liu <jasonliu2468@hotmail.com>
2025-08-05 23:08:09 -0400

    Fix ticket_bot_trade decorator

--- src/commands/ticket_bot/ticket_bot_trade.py
+++ src/commands/ticket_bot/ticket_bot_trade.py
@@ -21,23 +21,29 @@ class TicketBotTrade(app_commands.Group):
     ####################################################################################
     ################################### COMMANDS #######################################
     ####################################################################################
-    @app_commands(name="accept", description="Accept a user's trade request")
+    @app_commands.command(name="accept", description="Accept a user's trade request")
     async def trade_accept(self, interaction: discord.Interaction):
         await handle_command(self.handle_accept, interaction, self.__allowed_roles)
 
-    @app_commands(name="reject", description="Reject a user's trade request")
+    @app_commands.command(name="reject", description="Reject a user's trade request")
     async def trade_reject(self, interaction: discord.Interaction):
         await handle_command(self.handle_reject, interaction, self.__allowed_roles)
 
-    @app_commands(name="cancel", description="Cancels an existing trade request")
+    @app_commands.command(
+        name="cancel", description="Cancels an existing trade request"
+    )
     async def trade_cancel(self, interaction: discord.Interaction):
         await handle_command(self.handle_cancel, interaction, self.__allowed_roles)
 
-    @app_commands(name="start", description="Request to trade with another user")
+    @app_commands.command(
+        name="start", description="Request to trade with another user"
+    )
     async def trade_start(self, interaction: discord.Interaction):
         await handle_command(self.handle_start, interaction, self.__allowed_roles)
 
-    @app_commands(name="coinflip", description="Coinflip for tickets with another user")
+    @app_commands.command(
+        name="coinflip", description="Coinflip for tickets with another user"
+    )
     async def trade_coinflip(self, interaction: discord.Interaction):
         await handle_command(self.handle_coinflip, interaction, self.__allowed_roles)
 
