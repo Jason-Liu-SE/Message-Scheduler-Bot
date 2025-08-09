639043c7a2bf24d12758ac645e84c68b76f5ddd1
Jason Liu <jasonliu2468@hotmail.com>
2025-08-08 21:22:45 -0400

    Add a ternary action view

---
+++ src/ui/ticket_bot/ternary_action.py
@@ -0,0 +1,82 @@
+from typing import Awaitable, Callable
+import discord
+
+from ui.common.view_wrapper import ViewWrapper
+
+
+class TernaryActionView(ViewWrapper):
+    def __init__(
+        self,
+        timeout: int = 60,
+        primary_label: str = "Primary",
+        secondary_label: str = "Secondary",
+        danger_label: str = "Danger",
+        primary_style: discord.ButtonStyle = discord.ButtonStyle.primary,
+        secondary_style: discord.ButtonStyle = discord.ButtonStyle.secondary,
+        danger_style: discord.ButtonStyle = discord.ButtonStyle.danger,
+        primary_cb: (
+            Callable[[discord.Interaction, discord.ui.Button], Awaitable[None]] | None
+        ) = None,
+        secondary_cb: (
+            Callable[[discord.Interaction, discord.ui.Button], Awaitable[None]] | None
+        ) = None,
+        danger_cb: (
+            Callable[[discord.Interaction, discord.ui.Button], Awaitable[None]] | None
+        ) = None,
+        authorized_ids: list[int] = [],
+    ):
+        super().__init__(timeout=timeout, authorized_ids=authorized_ids)
+
+        self.primary_cb = primary_cb
+        self.secondary_cb = secondary_cb
+        self.danger_cb = danger_cb
+
+        self.btn_primary.label = primary_label
+        self.btn_secondary.label = secondary_label
+        self.btn_danger.label = danger_label
+
+        self.btn_primary.style = primary_style
+        self.btn_secondary.style = secondary_style
+        self.btn_danger.style = danger_style
+
+    #########################################################################################
+    ################################## Interactables ########################################
+    #########################################################################################
+    @discord.ui.button(label="Primary", style=discord.ButtonStyle.primary)
+    async def btn_primary(
+        self, interaction: discord.Interaction, btn: discord.ui.Button
+    ) -> None:
+        await self.handle_interaction(interaction, self.handle_primary, btn)
+
+    @discord.ui.button(label="Secondary", style=discord.ButtonStyle.secondary)
+    async def btn_secondary(
+        self, interaction: discord.Interaction, btn: discord.ui.Button
+    ) -> None:
+        await self.handle_interaction(interaction, self.handle_secondary, btn)
+
+    @discord.ui.button(label="Danger", style=discord.ButtonStyle.danger)
+    async def btn_danger(
+        self, interaction: discord.Interaction, btn: discord.ui.Button
+    ) -> None:
+        await self.handle_interaction(interaction, self.handle_danger, btn)
+
+    #########################################################################################
+    ##################################### Handlers ##########################################
+    #########################################################################################
+    async def handle_primary(
+        self, interaction: discord.Interaction, btn: discord.ui.Button
+    ) -> None:
+        if self.primary_cb:
+            await self.primary_cb(interaction, btn)
+
+    async def handle_secondary(
+        self, interaction: discord.Interaction, btn: discord.ui.Button
+    ) -> None:
+        if self.secondary_cb:
+            await self.secondary_cb(interaction, btn)
+
+    async def handle_danger(
+        self, interaction: discord.Interaction, btn: discord.ui.Button
+    ) -> None:
+        if self.danger_cb:
+            await self.danger_cb(interaction, btn)
